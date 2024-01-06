5800cfad0443c5c2917fc5dcc4296e62c8cd481c
Thomas Stephen Lee <lee@krittika.fossee.in>
2024-01-06 12:09:56 +0530

    GitHub now.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https//astro.web.fossee.in',
 	integrations: [mdx(), sitemap()],
 });
