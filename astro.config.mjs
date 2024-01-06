454c6f1ceca8814b6230d5092ddb29262e7645ec
Thomas Stephen Lee <lee@krittika.fossee.in>
2024-01-06 12:34:30 +0530

    Fix typo.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https//astro.web.fossee.in',
+	site: 'http://astro.web.fossee.in',
 	integrations: [mdx(), sitemap()],
 });
